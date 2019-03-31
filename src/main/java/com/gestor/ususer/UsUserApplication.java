49a5f54e368863a66eb77a168cb4670bce2c1200
Juan Condori <juan.condori@inkafarmadigital.pe>
2019-03-31 17:35:10 -0500

    main pr

---
+++ src/main/java/com/gestor/ususer/UsUserApplication.java
@@ -0,0 +1,13 @@
+package com.gestor.ususer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gestor/ususer/UsUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gestor.ususer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsUserApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
