8abce2ca5d2986a9b8f1b972e096180677fc61b5
Juan Condori <juan.condori@inkafarmadigital.pe>
2019-04-01 23:25:58 -0500

    adding services

--- src/main/java/com/gestor/ususer/model/Usuario.java
+++ src/main/java/com/gestor/ususer/model/Usuario.java
@@ -1,9 +1,12 @@
 package com.gestor.ususer.model;
 
+
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class Usuario {
@@ -13,8 +16,13 @@ public class Usuario {
 	private long id;
 
 	private String username;
-
 	private String password;
+	private String name;
+	private String lastName;
+	private Integer age;
+	private Date birthDate;
+
+
 
 	public long getId() {
 		return id;
@@ -34,5 +42,37 @@ public class Usuario {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
 	}
 }
